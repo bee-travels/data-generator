082ed9ffd6ee0df485d1d106a4f788acca0b72da
Mofizur Rahman <moficodes@gmail.com>
2020-04-20 17:26:42 -0400

    updated hotel data with id

---
+++ src/hotels/couchdb_upload_data.py
@@ -0,0 +1,47 @@
+import couchdb
+import json
+import sys
+
+
+def create_db(couch, db_name):
+    try:
+        db = couch.create(db_name)
+        return db
+    except Exception as e:
+        print("DB Not created")
+        print(e)
+
+
+def get_db(couch, db_name):
+    try:
+        db = couch[db_name]
+        return db
+    except Exception as e:
+        print("DB not found")
+        print(e)
+    return create_db(couch, db_name)
+
+
+def load_json(file_name):
+    with open(file_name) as json_data:
+        return json.load(json_data)
+
+
+def bulk_load_data(db, json_data, upload_name):
+    print(upload_name)
+    for data in json_data:
+        db.save(data)
+    print(upload_name + " completed")
+
+
+def main():
+    couch = couchdb.Server("http://admin:CdjLgHqzVWmUPuic8lvE@127.0.0.1:5984/")
+    hotel_db = get_db(couch, "hotel")
+    hotel_data = load_json("hotel-data.json")
+    bulk_load_data(hotel_db, hotel_data, "Hotel Data")
+    hotel_info = load_json("hotel-info.json")
+    info_db = get_db(couch, "hotel-info")
+    bulk_load_data(info_db, hotel_info, "Hotel Info")
+
+if __name__ == "__main__":
+    main()
---
+++ src/hotels/hotels_data_gen.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python3
+import csv
+import random
+import json
+import math
+import os
+
+# array of tuples. (type, relative_number)
+# if the relative_number s are 3, 2, 1 type 1 will be 3 times as likely as type 3
+hotel_type = [("budget", 2), ("comfort", 1), ("luxury", 1)]
+
+
+def generate_data_for_destination(filename, hotels, image_urls):
+    hotel_base_cost = 100
+    csvfile = open(filename, "rt")
+    reader = csv.reader(csvfile, delimiter=',')
+    hotel_data = []
+
+    for row in reader:
+        cityname = row[0]
+        country = row[3]
+        population = int(row[5])
+        cost_of_living_index = float(row[4])
+        upper_limit = random.randint(25, 35)
+        num_hotels = int(min(math.floor(population / 100000), upper_limit))
+        hotels_in_city = []
+        for i in range(num_hotels):
+            hotels_in_city.append(
+                get_hotel(city=cityname,
+                          country=country,
+                          hotels=hotels,
+                          hotel_type=get_hotel_type(hotel_type),
+                          image_urls=get_image_urls_subset(image_urls),
+                          col_index=cost_of_living_index,
+                          base_cost=hotel_base_cost)
+            )
+        hotel_data.extend(hotels_in_city)
+    
+    for i in range(len(hotel_data)):
+        hotel_data[i]["id"] = i+1
+
+    return hotel_data
+    csvfile.close()
+
+
+def write_json_to_file(json_data, file_name):
+    with open(file_name, "w+") as f:
+        json.dump(json_data, f, ensure_ascii=True, indent=2)
+
+
+def generate_list_from_file(filename):
+    data = []
+    with open(filename) as f:
+        for line in f:
+            data.append(line.strip())
+    return data
+
+
+def get_hotel_type(hotel_type):
+    new_list = []
+    for data in hotel_type:
+        new_list = new_list + [data[0]] * data[1]
+
+    return random.choice(new_list)
+
+
+def get_hotels(hotel_list, superchain):
+    hotels = {}
+    for hotel in hotel_list:
+        superchain_choice = superchain[random.randint(0, len(superchain) - 1)]
+        # print(superchain[superchain_choice])
+        hotel_group = get_hotel_type(hotel_type)
+        if not hotel_group in hotels:
+            hotels[hotel_group] = {}
+        if not superchain_choice in hotels[hotel_group]:
+            hotels[hotel_group][superchain_choice] = []
+        hotels[hotel_group][superchain_choice].append(hotel)
+    return hotels
+
+
+def get_price_multiplier(hotel_type):
+    if hotel_type == "budget":
+        return 1
+    elif hotel_type == "comfort":
+        return 1.25
+    else:
+        return 2
+
+
+def get_image_urls_subset(image_urls):
+    indexes = set()
+    urls = []
+    count = random.randint(9, 12)
+    for i in range(count):
+        indexes.add(random.randint(0, len(image_urls) - 1))
+    for index in list(indexes):
+        urls.append(image_urls[index])
+    return urls
+
+
+def get_hotel(city, country, hotels, hotel_type, image_urls, col_index=1.0, base_cost=100):
+    # col_index is the cost of living index for each city
+    hotel = {}
+    superchain = random.choice(list(hotels[hotel_type].keys()))
+    hotel["city"] = city
+    hotel["country"] = country
+    hotel["superchain"] = superchain
+    hotel["name"] = random.choice(hotels[hotel_type][superchain])
+    hotel["type"] = hotel_type
+    hotel["cost"] = math.ceil(get_price_multiplier(
+        hotel_type) * (random.uniform(1, 1.5)) * col_index * base_cost)
+    hotel["images"] = image_urls
+    return hotel
+
+def flatten_hotels(hotels):
+    hotel = []
+    id = 1
+    for t, ss in hotels.items():
+        for s, htls in ss.items():
+            for h in htls:
+                htl = {}
+                htl["name"] = h
+                htl["superchain"] = s
+                htl["type"] = t
+                htl["id"] = id
+                id = id+1
+                hotel.append(htl)
+    return hotel
+
+def generate_hotel_id(hotel_data, hotel_list):
+    for i in range(len(hotel_data)):
+        for hotel in hotel_list:
+            if hotel_data[i]["name"] == hotel["name"]:
+                hotel_data[i]["hotel_id"] = hotel["id"]
+    return hotel_data
+
+def generate_data():
+    hotel_list = generate_list_from_file("hotel_names.txt")
+    superchain = generate_list_from_file("superchain_names.txt")
+    image_urls = generate_list_from_file("urls.txt")
+
+    # print(get_image_urls_subset(image_urls))
+
+    hotels = get_hotels(hotel_list, superchain)
+    hotel = flatten_hotels(hotels)
+    # for i in range(10):
+    #     print(json.dumps(get_hotel(hotels, "comfort", image_urls), indent=4))
+
+    hotel_data = generate_data_for_destination(
+        "cities.csv", hotels, image_urls)
+    
+    hotel_data = generate_hotel_id(hotel_data, hotel)
+
+    write_json_to_file(hotel_data, "hotel-data.json")
+    write_json_to_file(hotel, "hotel-info.json")
+
+def main():
+    generate_data()
+
+if __name__ == "__main__":
+    main()
