6a64105bc86e29142506b923d851e895996d8f8e
Mofizur Rahman <moficodes@gmail.com>
2020-04-26 03:58:42 -0400

    minor fix

--- src/car_rental/mongo_upload_data.py
+++ src/car_rental/mongo_upload_data.py
@@ -3,6 +3,8 @@ import sys
 import json
 import os
 import utils
+import logging
+
 
 def get_mongo_client():
     try:
@@ -24,19 +26,21 @@ def delete_existing_collection(db, collection_name):
     except Exception as e:
         logging.warning("could not delete db ", collection_name)
 
+
 def populate_mongo():
     car_data = utils.load_json("cars.json")
     car_info_data = utils.load_json("car-info.json")
-    
-    mongoHotels = get_mongo_client()
-    db = mongoHotels.beetravels
-    delete_existing_collection(db, "hotel_info")
-    delete_existing_collection(db, "hotels")
+
+    client = get_mongo_client()
+    db = client.beetravels
+    delete_existing_collection(db, "car_info")
+    delete_existing_collection(db, "cars")
 
     logging.info("inserting data")
     db.car_info.insert_many(car_info_data)
     db.cars.insert_many(car_data)
     logging.info("done inserting documents")
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
