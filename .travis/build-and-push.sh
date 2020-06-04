acd1b73ef976300e22247e0ad4155b1a3b070f9e
Mofizur Rahman <moficodes@gmail.com>
2020-06-04 08:15:13 -0400

    travis build

---
+++ .travis/build-and-push.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker build -t beetravels/data-gen-destination:$TRAVIS_COMMIT src/destination
+docker build -t beetravels/data-gen-carrental:$TRAVIS_COMMIT src/carrental
+docker build -t beetravels/data-gen-hotel:$TRAVIS_COMMIT src/hotel
+docker build -t beetravels/data-gen-airports:$TRAVIS_COMMIT src/airports
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+docker push beetravels/data-gen-destination:$TRAVIS_COMMIT
+docker push beetravels/data-gen-carrental:$TRAVIS_COMMIT
+docker push beetravels/data-gen-hotel:$TRAVIS_COMMIT
+docker push beetravels/data-gen-airports:$TRAVIS_COMMIT
