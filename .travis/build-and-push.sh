da903a1467bae812c9340ba38f7de931c4893fd5
Mofizur Rahman <moficodes@gmail.com>
2020-06-04 09:10:20 -0400

    update docker file

--- .travis/build-and-push.sh
+++ .travis/build-and-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker build -t beetravels/data-gen-destination:$TRAVIS_COMMIT src/destination
-docker build -t beetravels/data-gen-carrental:$TRAVIS_COMMIT src/carrental
-docker build -t beetravels/data-gen-hotel:$TRAVIS_COMMIT src/hotel
+docker build -t beetravels/data-gen-carrental:$TRAVIS_COMMIT src/car_rental
+docker build -t beetravels/data-gen-hotel:$TRAVIS_COMMIT src/hotels
 docker build -t beetravels/data-gen-airports:$TRAVIS_COMMIT src/airports
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
