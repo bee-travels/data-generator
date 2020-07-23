34a5ce0cd24253bd82c00b9af0f3ef665afbf459
mhrosen-ibm <67337467+mhrosen-ibm@users.noreply.github.com>
2020-07-23 19:52:35 -0400

    transactions.json is a thing now

--- src/transactions/transaction_data_gen.py
+++ src/transactions/transaction_data_gen.py
@@ -286,7 +286,9 @@ def generate_user_car(car_full_url, priority, party_size):
 
 def format_postgres(one_user_dict, hotel_full_url, car_full_url, priority, party_size):
     string = ""
-    initialized_dict = {"totalAmount": 0.00, "currency": "USD", "status": "unprocessed", "cartItems": [{}], "billingDetails": {
+    date_today = datetime.datetime.now()
+    time_stamp = date_today.isoformat()
+    initialized_dict = {"totalAmount": 0.00, "time_stamp": time_stamp, "currency": "USD", "status": "unprocessed", "cartItems": [{}], "billingDetails": {
     }, "paymentMethodDetails": {"type": "Card", "creditcardNumber": "0000 0000 0000 0000", "expMonth": 1, "expYear": 2050, "cvc": "0000"}}
     if "cars" in car_full_url:  # for the future in case we provide a blank string when people do not purchase hotel and car at same time
         carjson = generate_user_car(car_full_url, priority, party_size)
@@ -300,12 +302,12 @@ def format_postgres(one_user_dict, hotel_full_url, car_full_url, priority, party
             dateTo = car_full_url.split(
                 "&")[-1].split("=")[1].replace("-", " ")
             yearTo, monthTo, dayTo = dateTo.split(" ")
-            string = monthTo + " " + dayTo + " " + yearTo
+            string = monthTo + "-" + dayTo + "-" + yearTo
             cartItemsCar_dict["endDate"] = string
             dateFrom = car_full_url.split(
                 "&")[-2].split("=")[1].replace("-", " ")
             yearFrom, monthFrom, dayFrom = dateFrom.split(" ")
-            string = monthFrom + " " + dayFrom + " " + yearFrom
+            string = monthFrom + "-" + dayFrom + "-" + yearFrom
             cartItemsCar_dict["startDate"] = string
             billingDetails = initialized_dict["billingDetails"]
             # CAN WE ASSUME NAMES ARE ALWAYS JUST 2 WORDS???)
@@ -331,12 +333,12 @@ def format_postgres(one_user_dict, hotel_full_url, car_full_url, priority, party
             dateTo_hotel = car_full_url.split(
                 "&")[-1].split("=")[1].replace("-", " ")
             yearTo, monthTo, dayTo = dateTo_hotel.split(" ")
-            string = monthTo + " " + dayTo + " " + yearTo
+            string = monthTo + "-" + dayTo + "-" + yearTo
             cartItemsHotel_dict["endDate"] = string
             dateFrom_hotel = car_full_url.split(
                 "&")[-2].split("=")[1].replace("-", " ")
             yearFrom, monthFrom, dayFrom = dateFrom_hotel.split(" ")
-            string = monthFrom + " " + dayFrom + " " + yearFrom
+            string = monthFrom + "-" + dayFrom + "-" + yearFrom
             cartItemsHotel_dict["startDate"] = string
 
         try:
@@ -361,6 +363,7 @@ def posting(params):
 
 def main():
     counter = 0
+    result = []
     for _ in range(10000):  # change back to 100
         total = 0
         for user in users:
@@ -439,76 +442,77 @@ def main():
             postgres = format_postgres(
                 user, hotel_full_url, car_full_url, priority, party_size)
             # POSTGRES SETS DATA INTO READABLE FORMAT FOR THE POSTGRES DATABASE
-            posting(postgres)
+            result.append(postgres)
+            # posting(postgres)
             # POSTS TO POSTGRES DATABASE
         counter += 1
         if counter == 1:
             print("iteration: 1")
         if counter % 100 == 0:
             print("iteration: ", counter)
-
-        # date depending on reason -> 3 - 30 days for business, 20 - 60 for leisure
+    utils.write_json_to_file(result, "transactions.json")
+    # date depending on reason -> 3 - 30 days for business, 20 - 60 for leisure
 ###########################Generation################################################
-        # business: 65% of time traveling 1-19 days in advanced,
-        # ^^^5/9 of time is 1 person
-        # so, 13% of time 20-30 days in advance with 1 person
-        # 11% of time 20-30 days in advanced with 2-3 people
-        # 5% of the time 20-30 days in advanced with 4 or more people
-        # 6% of the time 30-60 days in advanced
+    # business: 65% of time traveling 1-19 days in advanced,
+    # ^^^5/9 of time is 1 person
+    # so, 13% of time 20-30 days in advance with 1 person
+    # 11% of time 20-30 days in advanced with 2-3 people
+    # 5% of the time 20-30 days in advanced with 4 or more people
+    # 6% of the time 30-60 days in advanced
 
-        # leisure: 35% of time traveling 1-19 days in advanced
-        #
+    # leisure: 35% of time traveling 1-19 days in advanced
+    #
 # Extra criteria for leisure:
 # if traveling more than 1 week then leisure
 #####################################################################################
 
 
 #########################Recommendation################################################
-        # buisness:
-        # --> will choose recommendation based on what most business people choose. Based on income >200000, will choose the fastest option for
-        # for inbetween, randomly select OR choose middle priced options
-        # flights and most expensive option for time/luxury.. For budget (<100000), choose least expensive recommendations
+    # buisness:
+    # --> will choose recommendation based on what most business people choose. Based on income >200000, will choose the fastest option for
+    # for inbetween, randomly select OR choose middle priced options
+    # flights and most expensive option for time/luxury.. For budget (<100000), choose least expensive recommendations
 
-        # business tags for hotel/car:
+    # business tags for hotel/car:
 
-        # more likely to be buisness if traveling with (1. leaving within 3-30 days in advance (2. also, 1 person
-        # if traveling 1-19 days in advanced, 85% chance business.
-        #           unless income > 200,000, 90% chance of being time (time is a rich business man); select highest price
-        # if traveling 20-30 days in advanced 60% chance business and 1 person
-        #           income > 200,000, 60% chance of being time (time is a rich business man)
-        # elif traveling 20-30 days in advanced 40% chance business and 2-3 people
-        #           income > 200,000, still 40% chance of being time (time is a rich business man)
-        # elif traveling 20-30 days in advanced 10% chance business and 4 or more people
-        #           income > 200,000, still 40% chance of being time (time is a rich business man)
-        # if traveling 30-60 days in advanced, 15% chance of business
+    # more likely to be buisness if traveling with (1. leaving within 3-30 days in advance (2. also, 1 person
+    # if traveling 1-19 days in advanced, 85% chance business.
+    #           unless income > 200,000, 90% chance of being time (time is a rich business man); select highest price
+    # if traveling 20-30 days in advanced 60% chance business and 1 person
+    #           income > 200,000, 60% chance of being time (time is a rich business man)
+    # elif traveling 20-30 days in advanced 40% chance business and 2-3 people
+    #           income > 200,000, still 40% chance of being time (time is a rich business man)
+    # elif traveling 20-30 days in advanced 10% chance business and 4 or more people
+    #           income > 200,000, still 40% chance of being time (time is a rich business man)
+    # if traveling 30-60 days in advanced, 15% chance of business
 
-        # leisure:
-        # --> will choose recommendation based on what most leisure people choose. Based on income >200000, will choose the luxury option
-        # for inbetween, randomly select OR choose middle priced options
-        # For budget (<100000), choose least expensive recommendations
+    # leisure:
+    # --> will choose recommendation based on what most leisure people choose. Based on income >200000, will choose the luxury option
+    # for inbetween, randomly select OR choose middle priced options
+    # For budget (<100000), choose least expensive recommendations
 
-        # more likely to be leisure if (1. leaving 20-60 days in advanced (2. also 2 people or more
-        # if traveling 1-19 days in advance, 15% chance of being leisure. if also income > 200,000 then only 10% chance of leisure
-        # if traveling 20-30 days in advance and 1 person, only 40% chance of being leisure
-        # if traveling 20-30 days in advance and 2-3 people, 60% chance of being leisure
-        # if traveling 20-30 days in advance and 4 or more people, 90% chance of being leisure
-        # if traveling 30-60 days in advanced, 85% chance of leisure
+    # more likely to be leisure if (1. leaving 20-60 days in advanced (2. also 2 people or more
+    # if traveling 1-19 days in advance, 15% chance of being leisure. if also income > 200,000 then only 10% chance of leisure
+    # if traveling 20-30 days in advance and 1 person, only 40% chance of being leisure
+    # if traveling 20-30 days in advance and 2-3 people, 60% chance of being leisure
+    # if traveling 20-30 days in advance and 4 or more people, 90% chance of being leisure
+    # if traveling 30-60 days in advanced, 85% chance of leisure
 
-        # time
-        # leave also 7-14 days in advanced with high income > 200000: will select highest priced hotel within a 2 week timeline.
-        #    Will select the shortest duration flight, then most expensive
-        # luxury
-        # leave 30-60 days in
-        # budget
-        # will select cheapest
+    # time
+    # leave also 7-14 days in advanced with high income > 200000: will select highest priced hotel within a 2 week timeline.
+    #    Will select the shortest duration flight, then most expensive
+    # luxury
+    # leave 30-60 days in
+    # budget
+    # will select cheapest
 
-        # using same randnum for each selection
-        # carRatio = (float(carloyal) / total) * 100.0
-        # flightRatio = (float(flightloyal) / total) * 100.0
+    # using same randnum for each selection
+    # carRatio = (float(carloyal) / total) * 100.0
+    # flightRatio = (float(flightloyal) / total) * 100.0
 
-        # print("Car Ratio: ", carRatio)
-        # print("Flight Ratio: ", flightRatio)
-        # print()
+    # print("Car Ratio: ", carRatio)
+    # print("Flight Ratio: ", flightRatio)
+    # print()
 
 main()
 
--- src/transactions/utils.py
+++ src/transactions/utils.py
@@ -1,4 +1,14 @@
 import json
+
+
 def load_json(file_name):
     with open(file_name) as json_data:
         return json.load(json_data)
+
+
+def write_json_to_file(json_data, file_name, minify=False):
+    with open(file_name, "w+") as f:
+        if minify:
+            f.write(json.dumps(json_data))
+            return
+        json.dump(json_data, f, ensure_ascii=True, indent=2)
