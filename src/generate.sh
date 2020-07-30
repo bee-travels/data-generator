5a20fb8606dc77af0feb66275cf2b9afdf207058
Max Shapiro <magicmax33@gmail.com>
2020-07-30 10:42:40 -0400

    Update generate.sh

--- src/generate.sh
+++ src/generate.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 
-mongoCreds () {
+dockerTag=57d4b5fdb837d1b7f05f2a9d451b4310309b95c5
+
+mongoInitCreds() {
+	read -p "  Database Connection URL: " mongoURL
+	read -p "  Use SSL/TLS (Y/N): " usesTLS
+	if [ "$usesTLS" = "Y" ] || [ "$usesTLS" = "y" ]; then
+		read -p "  Certificate File Path: " mongoCertificatePath
+		mongoCertificate=$(cat $mongoCertificatePath)
+	else
+		unset mongoCertificate
+	fi
+}
+
+mongoCreds() {
 	if [ "$mongoURL" ]; then
 		read -p "  Use Existing Mongo Credentials (Y/N): " wantExistingMongoCreds
 		if [ "$wantExistingMongoCreds" = "N" ] || [ "$wantExistingMongoCreds" = "n" ]; then
-			read -p "  Database Connection URL: " mongoURL
+			mongoInitCreds
 		fi
 	else
-		read -p "  Database Connection URL: " mongoURL
+		mongoInitCreds
+	fi
+}
+
+postgresInitCreds() {
+	read -p "  Host: " postgresHost
+	read -p "  Port: " postgresPort
+	read -p "  User: " postgresUser
+	read -p "  Password: " postgresPassword
+	read -p "  Use SSL/TLS (Y/N): " usesTLS
+	if [ "$usesTLS" = "Y" ] || [ "$usesTLS" = "y" ]; then
+		read -p "  Certificate File Path: " postgresCertificatePath
+		postgresCertificate=$(cat $postgresCertificatePath)
+		read -p "  Database: " postgresDB
+	else
+		unset postgresCertificate
 	fi
 }
 
@@ -15,31 +43,32 @@ postgresCreds() {
 	if [ "$postgresHost" ]; then
 		read -p "  Use Existing Postgres Credentials (Y/N): " wantExistingPostgresCreds
 		if [ "$wantExistingPostgresCreds" = "N" ] || [ "$wantExistingPostgresCreds" = "n" ]; then
-			read -p "  User: " postgresUser
-			read -p "  Host: " postgresHost
-			read -p "  Password: " postgresPassword
+			postgresInitCreds
 		fi
 	else
-		read -p "  User: " postgresUser
-		read -p "  Host: " postgresHost
-		read -p "  Password: " postgresPassword
+		postgresInitCreds
 	fi
 }
 
-couchCreds () {
+couchInitCreds() {
+	read -p "  Database Connection URL: " couchURL
+}
+
+couchCreds() {
 	if [ "$couchURL" ]; then
 		read -p "  Use Existing Couch/Cloudant Credentials (Y/N): " wantExistingCouchCreds
 		if [ "$wantExistingCouchCreds" = "N" ] || [ "$wantExistingCouchCreds" = "n" ]; then
-			read -p "  Database Connection URL: " couchURL
+			couchInitCreds
 		fi
 	else
-		read -p "  Database Connection URL: " couchURL
+		couchInitCreds
 	fi
 }
 
 
 echo "Welcome to the Bee Travels Data Generating Script"
 echo "Please answer the following options to configure your data:"
+
 echo ""
 read -p "Destination Data (Y/N): " wantsDestinationData
 if [ "$wantsDestinationData" = "Y" ] || [ "$wantsDestinationData" = "y" ]; then
@@ -49,25 +78,34 @@ if [ "$wantsDestinationData" = "Y" ] || [ "$wantsDestinationData" = "y" ]; then
 	else
 		generateDestinationData=false
 	fi
-	read -p "  Database (mongodb/postgresdb/couchdb/cloudant): " destinationDatabase
+	read -p "  Database (mongodb/postgres/couchdb/cloudant): " destinationDatabase
 	if [ "$destinationDatabase" = "mongodb" ]; then
 		mongoCreds
 		echo ""
 		echo "Starting Destination data process"
-		docker run --net host -e GENERATE_DATA=$generateDestinationData -e DATABASE=mongodb -e MONGO_CONNECTION_URL=$mongoURL beetravels/data-gen-destination:v2.0.0
-	elif [ "$destinationDatabase" = "postgresdb" ]; then
+		if [ "$mongoCertificate" ]; then
+			docker run --net host -e GENERATE_DATA=$generateDestinationData -e DATABASE=mongodb -e DATABASE_CERT="$mongoCertificate" -e MONGO_CONNECTION_URL=$mongoURL beetravels/data-gen-destination:$dockerTag
+		else
+			docker run --net host -e GENERATE_DATA=$generateDestinationData -e DATABASE=mongodb -e MONGO_CONNECTION_URL=$mongoURL beetravels/data-gen-destination:$dockerTag
+		fi
+	elif [ "$destinationDatabase" = "postgres" ]; then
 		postgresCreds
 		echo ""
 		echo "Starting Destination data process"
-		docker run --net host -e GENERATE_DATA=$generateDestinationData -e DATABASE=postgresdb -e PG_USER=$postgresUser -e PG_HOST=$postgresHost -e PG_PASSWORD=$postgresPassword beetravels/data-gen-destination:v2.0.0
+		if [ "$postgresCertificate" ]; then
+			docker run --net host -e GENERATE_DATA=$generateDestinationData -e DATABASE=postgres -e DATABASE_CERT="$postgresCertificate" -e PG_HOST=$postgresHost -e PG_PORT=$postgresPort -e PG_USER=$postgresUser -e PG_PASSWORD=$postgresPassword -e PG_DB=$postgresDB beetravels/data-gen-destination:$dockerTag
+		else
+			docker run --net host -e GENERATE_DATA=$generateDestinationData -e DATABASE=postgres -e PG_HOST=$postgresHost -e PG_PORT=$postgresPort -e PG_USER=$postgresUser -e PG_PASSWORD=$postgresPassword beetravels/data-gen-destination:$dockerTag
+		fi
 	elif [ "$destinationDatabase" = "couchdb" ] || [ "$destinationDatabase" = "cloudant" ]; then
 		couchCreds
 		echo ""
 		echo "Starting Destination data process"
-		docker run --net host -e GENERATE_DATA=$generateDestinationData -e DATABASE=couchdb -e COUCH_CONNECTION_URL=$couchURL beetravels/data-gen-destination:v2.0.0
+		docker run --net host -e GENERATE_DATA=$generateDestinationData -e DATABASE=couchdb -e COUCH_CONNECTION_URL=$couchURL beetravels/data-gen-destination:$dockerTag
 	fi
 	echo "Destination data process complete"
 fi
+
 echo ""
 read -p "Hotel Data (Y/N): " wantsHotelData
 if [ "$wantsHotelData" = "Y" ] || [ "$wantsHotelData" = "y" ]; then
@@ -77,25 +115,34 @@ if [ "$wantsHotelData" = "Y" ] || [ "$wantsHotelData" = "y" ]; then
 	else
 		generateHotelData=false
 	fi
-	read -p "  Database (mongodb/postgresdb/couchdb/cloudant): " hotelDatabase
+	read -p "  Database (mongodb/postgres/couchdb/cloudant): " hotelDatabase
 	if [ "$hotelDatabase" = "mongodb" ]; then
 		mongoCreds
 		echo ""
 		echo "Starting Hotel data process"
-		docker run --net host -e GENERATE_DATA=$generateHotelData -e DATABASE=mongodb -e MONGO_CONNECTION_URL=$mongoURL beetravels/data-gen-hotel:v2.0.0
-	elif [ "$hotelDatabase" = "postgresdb" ]; then
+		if [ "$mongoCertificate" ]; then
+			docker run --net host -e GENERATE_DATA=$generateHotelData -e DATABASE=mongodb -e DATABASE_CERT="$mongoCertificate" -e MONGO_CONNECTION_URL=$mongoURL beetravels/data-gen-hotel:$dockerTag
+		else
+			docker run --net host -e GENERATE_DATA=$generateHotelData -e DATABASE=mongodb -e MONGO_CONNECTION_URL=$mongoURL beetravels/data-gen-hotel:$dockerTag
+		fi
+	elif [ "$hotelDatabase" = "postgres" ]; then
 		postgresCreds
 		echo ""
 		echo "Starting Hotel data process"
-		docker run --net host -e GENERATE_DATA=$generateHotelData -e DATABASE=postgresdb -e PG_USER=$postgresUser -e PG_HOST=$postgresHost -e PG_PASSWORD=$postgresPassword beetravels/data-gen-hotel:v2.0.0
+		if [ "$postgresCertificate" ]; then
+			docker run --net host -e GENERATE_DATA=$generateHotelData -e DATABASE=postgres -e DATABASE_CERT="$postgresCertificate" -e PG_HOST=$postgresHost -e PG_PORT=$postgresPort -e PG_USER=$postgresUser -e PG_PASSWORD=$postgresPassword -e PG_DB=$postgresDB beetravels/data-gen-hotel:$dockerTag
+		else
+			docker run --net host -e GENERATE_DATA=$generateHotelData -e DATABASE=postgres -e PG_HOST=$postgresHost -e PG_PORT=$postgresPort -e PG_USER=$postgresUser -e PG_PASSWORD=$postgresPassword beetravels/data-gen-hotel:$dockerTag
+		fi
 	elif [ "$hotelDatabase" = "couchdb" ] || [ "$hotelDatabase" = "cloudant" ]; then
 		couchCreds
 		echo ""
 		echo "Starting Hotel data process"
-		docker run --net host -e GENERATE_DATA=$generateHotelData -e DATABASE=couchdb -e COUCH_CONNECTION_URL=$couchURL beetravels/data-gen-hotel:v2.0.0
+		docker run --net host -e GENERATE_DATA=$generateHotelData -e DATABASE=couchdb -e COUCH_CONNECTION_URL=$couchURL beetravels/data-gen-hotel:$dockerTag
 	fi
 	echo "Hotel data process complete"
 fi
+
 echo ""
 read -p "Car Rental Data (Y/N): " wantsCarData
 if [ "$wantsCarData" = "Y" ] || [ "$wantsCarData" = "y" ]; then
@@ -105,24 +152,57 @@ if [ "$wantsCarData" = "Y" ] || [ "$wantsCarData" = "y" ]; then
 	else
 		generateCarData=false
 	fi
-	read -p "  Database (mongodb/postgresdb/couchdb/cloudant): " carDatabase
+	read -p "  Database (mongodb/postgres/couchdb/cloudant): " carDatabase
 	if [ "$carDatabase" = "mongodb" ]; then
 		mongoCreds
 		echo ""
 		echo "Starting Car Rental data process"
-		docker run --net host -e GENERATE_DATA=$generateCarData -e DATABASE=mongodb -e MONGO_CONNECTION_URL=$mongoURL beetravels/data-gen-cars:v2.0.0
-	elif [ "$carDatabase" = "postgresdb" ]; then
+		if [ "$mongoCertificate" ]; then
+			docker run --net host -e GENERATE_DATA=$generateCarData -e DATABASE=mongodb -e DATABASE_CERT="$mongoCertificate" -e MONGO_CONNECTION_URL=$mongoURL beetravels/data-gen-carrental:$dockerTag
+		else
+			docker run --net host -e GENERATE_DATA=$generateCarData -e DATABASE=mongodb -e MONGO_CONNECTION_URL=$mongoURL beetravels/data-gen-carrental:$dockerTag
+		fi
+	elif [ "$carDatabase" = "postgres" ]; then
 		postgresCreds
 		echo ""
 		echo "Starting Car Rental data process"
-		docker run --net host -e GENERATE_DATA=$generateCarData -e DATABASE=postgresdb -e PG_USER=$postgresUser -e PG_HOST=$postgresHost -e PG_PASSWORD=$postgresPassword beetravels/data-gen-cars:v2.0.0
+		if [ "$postgresCertificate" ]; then
+			docker run --net host -e GENERATE_DATA=$generateCarData -e DATABASE=postgres -e DATABASE_CERT="$postgresCertificate" -e PG_HOST=$postgresHost -e PG_PORT=$postgresPort -e PG_USER=$postgresUser -e PG_PASSWORD=$postgresPassword -e PG_DB=$postgresDB beetravels/data-gen-carrental:$dockerTag
+		else
+			docker run --net host -e GENERATE_DATA=$generateCarData -e DATABASE=postgres -e PG_HOST=$postgresHost -e PG_PORT=$postgresPort -e PG_USER=$postgresUser -e PG_PASSWORD=$postgresPassword beetravels/data-gen-carrental:$dockerTag
+		fi
 	elif [ "$carDatabase" = "couchdb" ] || [ "$carDatabase" = "cloudant" ]; then
 		couchCreds
 		echo ""
 		echo "Starting Car Rental data process"
-		docker run --net host -e GENERATE_DATA=$generateCarData -e DATABASE=couchdb -e COUCH_CONNECTION_URL=$couchURL beetravels/data-gen-cars:v2.0.0
+		docker run --net host -e GENERATE_DATA=$generateCarData -e DATABASE=couchdb -e COUCH_CONNECTION_URL=$couchURL beetravels/data-gen-carrental:$dockerTag
 	fi
 	echo "Car Rental data process complete"
 fi
+
+echo ""
+echo "WARNING: Flight Data will take a long time to upload"
+read -p "Flight Data (Y/N): " wantsFlightData
+if [ "$wantsFlightData" = "Y" ] || [ "$wantsFlightData" = "y" ]; then
+	read -p "  Generate Flight Data (Y/N): " wantsGenerateFlightData
+	if [ "$wantsGenerateFlightData" = "Y" ] || [ "$wantsGenerateFlightData" = "y" ]; then
+		generateFlightData=true
+	else
+		generateFlightData=false
+	fi
+	read -p "  Database (postgres): " flightDatabase
+	if [ "$flightDatabase" = "postgres" ]; then
+		postgresCreds
+		echo ""
+		echo "Starting Flight data process"
+		if [ "$postgresCertificate" ]; then
+			docker run --net host -e GENERATE_DATA=$generateFlightData -e DATABASE=postgres -e DATABASE_CERT="$postgresCertificate" -e PG_HOST=$postgresHost -e PG_PORT=$postgresPort -e PG_USER=$postgresUser -e PG_PASSWORD=$postgresPassword -e PG_DB=$postgresDB beetravels/data-gen-airports:$dockerTag
+		else
+			docker run --net host -e GENERATE_DATA=$generateFlightData -e DATABASE=postgres -e PG_HOST=$postgresHost -e PG_PORT=$postgresPort -e PG_USER=$postgresUser -e PG_PASSWORD=$postgresPassword beetravels/data-gen-airports:$dockerTag
+		fi
+	fi
+	echo "Flight data process complete"
+fi
+
 echo ""
 echo "Data generation process complete"
